76e77d5d4a13ae2555986b99bb39e9d5d2cd5ce0
Richard Gross <richard.gross@maibornwolff.de>
2020-12-08 21:05:00 +0100

    adds code for task01b

--- src/task01.js
+++ src/task01.js
@@ -1,18 +1,39 @@
-exports.task01 = function(lines){
+exports.task01a = function (lines) {
     const expenses = lines.split("\n").map(e => parseInt(e.trim()));
 
     let result;
     outer:
-    for(let i = 0; i < expenses.length; i++){
-        for(let j = 0; j < expenses.length; j++){
-            if(i === j)continue;
+        for (let i = 0; i < expenses.length; i++) {
+            for (let j = 0; j < expenses.length; j++) {
+                if (i === j) continue;
 
-            if(expenses[i] + expenses[j] === 2020){
-                console.log(expenses[i] +" "+ expenses[j]);
-                result = expenses[i] * expenses[j];
-                break outer;
+                if (expenses[i] + expenses[j] === 2020) {
+                    console.log(expenses[i] + " " + expenses[j]);
+                    result = expenses[i] * expenses[j];
+                    break outer;
+                }
             }
         }
-    }
+    return result;
+}
+
+exports.task01b = function (lines) {
+    const expenses = lines.split("\n").map(e => parseInt(e.trim()));
+
+    let result;
+    outer:
+        for (let i = 0; i < expenses.length; i++) {
+            for (let j = 0; j < expenses.length; j++) {
+                for (let k = 0; k < expenses.length; k++) {
+                    if (i === j || i === k || j === k ) continue;
+
+                    if (expenses[i] + expenses[j] + expenses[k] === 2020) {
+                        console.log(expenses[i] + " " + expenses[j] + " " + expenses[k]);
+                        result = expenses[i] * expenses[j] * expenses[k];
+                        break outer;
+                    }
+                }
+            }
+        }
     return result;
 }

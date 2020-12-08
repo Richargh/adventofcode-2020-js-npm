362770b2f9af859f0e014a9be245c5ecb8fbc3ba
Richard Gross <richard.gross@maibornwolff.de>
2020-12-08 20:56:24 +0100

    adds code for task01a

--- src/task01.js
+++ src/task01.js
@@ -1,3 +1,18 @@
-exports.task01 = function(){
-    console.log("supp");
+exports.task01 = function(lines){
+    const expenses = lines.split("\n").map(e => parseInt(e.trim()));
+
+    let result;
+    outer:
+    for(let i = 0; i < expenses.length; i++){
+        for(let j = 0; j < expenses.length; j++){
+            if(i === j)continue;
+
+            if(expenses[i] + expenses[j] === 2020){
+                console.log(expenses[i] +" "+ expenses[j]);
+                result = expenses[i] * expenses[j];
+                break outer;
+            }
+        }
+    }
+    return result;
 }

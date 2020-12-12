0a85439ed1b6ccd4e2666a5a109eabec5534f7db
Richard Gross <richard.gross@maibornwolff.de>
2020-12-12 18:13:34 +0100

    F [Task03]: adds task03a tree count

---
+++ src/task03.js
@@ -0,0 +1,37 @@
+exports.task03a = countTrees;
+
+function countTrees(text, slope) {
+    const grid = gridOf(text);
+    return grid.countTrees(slope);
+}
+
+function countTreesForSlope(text, slope) {
+}
+
+function gridOf(text) {
+    const rows = text.split("\n")
+        .map(line => line.trim());
+    return new Grid(rows);
+}
+
+class Grid {
+    constructor(rows) {
+        this.rows = rows
+    }
+
+    isTree(rowIndex, colIndex) {
+        const col = this.rows[rowIndex];
+        return col[colIndex % col.length] === "#";
+    }
+
+    countTrees(slope){
+        let col = slope.right;
+        let treeCount = 0;
+        for (let row = slope.down; row < this.rows.length; row += slope.down){
+            if(this.isTree(row, col))
+                treeCount++;
+            col += slope.right;
+        }
+        return treeCount;
+    }
+}
